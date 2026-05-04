f6337e08b9bd30f84e6b4250637015b7de200f3b
Colin Hayes <chayes1@dt-login01.delta.ncsa.illinois.edu>
2026-05-04 01:45:16 -0500

    clean up

--- zero_bench.py
+++ zero_bench.py
@@ -22,7 +22,6 @@ MODEL_CONFIGS = {
     "1.3B": dict(n_layer=24, n_head=16,  n_embd=2048),
     "2.7B": dict(n_layer=32, n_head=32,  n_embd=2560),
     "6.7B": dict(n_layer=32, n_head=32,  n_embd=4096),
-    "10B":  dict(n_layer=32, n_head=40,  n_embd=5120),
 }
 
 SCRIPT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "zero_run_config.py")
